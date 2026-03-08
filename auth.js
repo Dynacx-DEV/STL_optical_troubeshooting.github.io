75083c55b2289f91a3a0ce293e92fdd80ed59b76
Dynacx-DEV <developerdynacx@gmail.com>
2026-03-08 00:04:20 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
 function login(){
 
+console.log("botão clicado")
+
 let u = document.getElementById("user").value
 let p = document.getElementById("pass").value
 
@@ -7,12 +9,16 @@ let user = USERS.find(x => x.username === u && x.password === p)
 
 if(user){
 
+console.log("login ok")
+
 localStorage.setItem("sessionUser",u)
 localStorage.setItem("sessionRole",user.role)
 
-window.location = "dashboard.html"
+window.location="dashboard.html"
 
 }else{
+
+console.log("login falhou")
 
 document.getElementById("msg").innerText="Login inválido"
 
