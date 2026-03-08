18f694ebf084ccea0586e3650bf761827055e2c7
Dynacx-DEV <developerdynacx@gmail.com>
2026-03-08 18:06:49 -0300

    Update wiki.js

--- wiki.js
+++ wiki.js
@@ -1,11 +1,8 @@
 fetch("articles.json")
-
 .then(r=>r.json())
-
 .then(data=>{
 
 window.articles=data
-
 buildMenu(data)
 
 })
@@ -13,67 +10,63 @@ buildMenu(data)
 function buildMenu(data){
 
 let menu=document.getElementById("menu")
-
 menu.innerHTML=""
 
-let categories={}
+let tree={}
 
 data.forEach(a=>{
 
-if(!categories[a.category]){
+if(!tree[a.category]) tree[a.category]={}
 
-categories[a.category]={}
+if(!tree[a.category][a.subcategory]) tree[a.category][a.subcategory]=[]
 
-}
-
-if(!categories[a.category][a.subcategory]){
-
-categories[a.category][a.subcategory]=[]
-
-}
-
-categories[a.category][a.subcategory].push(a)
+tree[a.category][a.subcategory].push(a)
 
 })
 
-for(let cat in categories){
+for(let cat in tree){
 
 let catDiv=document.createElement("div")
-
-catDiv.innerHTML="<b>"+cat+"</b>"
-
-catDiv.className="menu-category"
+catDiv.className="tree-category"
+catDiv.innerHTML="▶ "+cat
 
 let subContainer=document.createElement("div")
-
-subContainer.style.display="none"
+subContainer.className="tree-sub"
 
 catDiv.onclick=()=>{
 
 subContainer.style.display=
-subContainer.style.display=="none"?"block":"none"
+subContainer.style.display==="none"?"block":"none"
 
 }
 
 menu.appendChild(catDiv)
+menu.appendChild(subContainer)
 
-for(let sub in categories[cat]){
+for(let sub in tree[cat]){
 
 let subDiv=document.createElement("div")
-
-subDiv.innerHTML=sub
-
-subDiv.className="menu-sub"
+subDiv.className="tree-subcategory"
+subDiv.innerHTML="▶ "+sub
 
 let artContainer=document.createElement("div")
+artContainer.className="tree-articles"
 
-categories[cat][sub].forEach(a=>{
+subDiv.onclick=()=>{
+
+artContainer.style.display=
+artContainer.style.display==="none"?"block":"none"
+
+}
+
+subContainer.appendChild(subDiv)
+subContainer.appendChild(artContainer)
+
+tree[cat][sub].forEach(a=>{
 
 let art=document.createElement("div")
-
+art.className="tree-article"
 art.innerText=a.title
-
-art.className="menu-article"
 
 art.onclick=()=>showArticle(a)
 
@@ -81,13 +74,7 @@ artContainer.appendChild(art)
 
 })
 
-subContainer.appendChild(subDiv)
-
-subContainer.appendChild(artContainer)
-
 }
-
-menu.appendChild(subContainer)
 
 }
 
@@ -95,43 +82,26 @@ menu.appendChild(subContainer)
 
 function showArticle(a){
 
+document.getElementById("breadcrumbs").innerHTML=
+
+`<span>${a.category}</span> › <span>${a.subcategory}</span> › <b>${a.title}</b>`
+
 let html=""
 
 html+=`<h2>${a.title}</h2>`
-
 html+=`<p><b>Problem:</b> ${a.problem}</p>`
 
 html+="<h3>Symptoms</h3>"
-
-a.symptoms.forEach(s=>{
-
-html+=`<li>${s}</li>`
-
-})
+a.symptoms.forEach(s=>html+=`<li>${s}</li>`)
 
 html+="<h3>Diagnostics</h3>"
-
-a.diagnostics.forEach(d=>{
-
-html+=`<li>${d}</li>`
-
-})
+a.diagnostics.forEach(d=>html+=`<li>${d}</li>`)
 
 html+="<h3>Resolution</h3>"
-
-a.resolution.forEach(r=>{
-
-html+=`<li>${r}</li>`
-
-})
+a.resolution.forEach(r=>html+=`<li>${r}</li>`)
 
 html+="<h3>Tags</h3>"
-
-a.tags.forEach(t=>{
-
-html+=`<span class="tag">${t}</span>`
-
-})
+a.tags.forEach(t=>html+=`<span class="tag">${t}</span>`)
 
 if(a.images){
 
@@ -164,12 +134,20 @@ document.getElementById("article").innerHTML=html
 function openImage(src){
 
 let overlay=document.createElement("div")
-
 overlay.className="image-overlay"
 
-overlay.innerHTML=`<img src="${src}" class="fullscreen">`
+overlay.innerHTML=`
 
-overlay.onclick=()=>overlay.remove()
+<div class="image-box">
+
+<button class="close-btn">X</button>
+
+<img src="${src}" class="fullscreen">
+
+</div>
+`
+
+overlay.querySelector(".close-btn").onclick=()=>overlay.remove()
 
 document.body.appendChild(overlay)
 
